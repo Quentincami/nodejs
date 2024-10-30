eb866e3198f1bb2b01f709fa34f244df226718e3
hugoblanc <hugoblanc.blend@gmail.com>
2024-10-30 17:33:24 +0100

    fix: implementation

--- index.ts
+++ index.ts
@@ -1,33 +1,34 @@
-import axios from 'axios';
-import { randomUUID } from 'crypto';
-import * as fs from 'fs/promises';
-import { join } from 'path';
-import { prompt } from './extraction_prompt';
+import { randomUUID } from "crypto";
+import dotenv from "dotenv";
+import * as fs from "fs/promises";
+import { join } from "path";
+import { prompt } from "./extraction_prompt";
 
+dotenv.config();
 
-const apikey = process.env.openaikey;
+import OpenAI from "openai";
 
+console.log(process.env["OPENAI_API_KEY"]);
 
-async function loadBase64ImageFromFile(
-  filepath: string,
-): Promise<string | undefined> {
+const client = new OpenAI({
+  apiKey: process.env["OPENAI_API_KEY"], // This is the default and can be omitted
+});
+
+async function loadBase64ImageFromFile(filepath: string): Promise<string | undefined> {
   try {
     const fileData = await fs.readFile(filepath);
-    const imageData = Buffer.from(fileData).toString('base64');
+    const imageData = Buffer.from(fileData).toString("base64");
     return imageData;
   } catch (error) {
-    console.error('Error loading image from file:', error);
+    console.error("Error loading image from file:", error);
   }
 }
 
-async function convertPptToPng(
-  filePath: string,
-  outputPath: string,
-): Promise<string[]> {
+async function convertPptToPng(filePath: string, outputPath: string): Promise<string[]> {
   try {
-    console.log('File path:', filePath);
+    console.log("File path:", filePath);
     await fs.mkdir(outputPath, { recursive: true });
-    const pptpng = (await import('ppt-png')).default;
+    const pptpng = (await import("ppt-png")).default;
 
     const converter = pptpng.create({
       files: [filePath],
@@ -40,7 +41,7 @@ async function convertPptToPng(
 
     await converter.convert();
     const files = await fs.readdir(outputPath);
-    const pngFiles = files.filter((file) => file.endsWith('.png'));
+    const pngFiles = files.filter((file) => file.endsWith(".png"));
 
     return pngFiles.map((file) => join(outputPath, file));
   } catch (error) {
@@ -49,56 +50,38 @@ async function convertPptToPng(
 }
 
 async function understandImageContent(b64Image: string) {
-  const headers = {
-    "Content-Type": "application/json",
-    "Authorization": `Bearer ${apikey}`
-  };
-
-  const payload = {
+  const response = await client.chat.completions.create({
     model: "gpt-4o",
     messages: [
       {
-        role:"system",
-        content:prompt
+        role: "system",
+        content: prompt,
       },
       {
-        role:"user",
+        role: "user",
         content: [
           {
-            type:"text",
-            text:"Analyze this image"
+            type: "text",
+            text: "Analyze this image",
           },
           {
-            type:"image_url",
+            type: "image_url",
             image_url: {
-              "url": "data:image/jpeg;base64,{base64_image}"
-            }
-          }
-        ]
-      }
-    ]
-  }
-  await axios.post('https://api.openai.com/v1/chat/completions', payload, { headers })
-    .then(response => {
-      const messageContent = response.data.choices[0].message.content;
-
-      const extractedText = messageContent
-          .filter((c: any) => c.type === 'text')   
-          .map((content: any) => content.text)
-          .join('\n');
-
-      console.log(extractedText); 
-      return extractedText;
-  })
-  .catch(error => {
-      console.error("Error:", error); // Handle error
+              url: `data:image/png;base64,${b64Image}`,
+            },
+          },
+        ],
+      },
+    ],
   });
+
+  const messageContent = response.choices[0];
+  return messageContent.message.content;
 }
 
 const uuid = randomUUID();
 const outputPath = `output/${uuid}/`;
-const filePath = "./omega.pptx"
-
+const filePath = "./omega.pptx";
 
 convertPptToPng(filePath, outputPath)
   .then((pngs) => {
@@ -108,8 +91,8 @@ convertPptToPng(filePath, outputPath)
         if (b64Image) {
           return understandImageContent(b64Image);
         }
-        return '';
-      }),
+        return "";
+      })
     )
       .then((results) => {
         //parentPort.postMessage({ success: true, content: results });
@@ -124,3 +107,4 @@ convertPptToPng(filePath, outputPath)
     //parentPort.postMessage({ success: false, error: error.message });
     console.error({ success: false, error: error.message });
   });
+
